7d2780f199f4ae04576f0ebac5c8c9bf571fa8b7
hanshan <tim.niven.public@gmail.com>
2020-12-22 13:43:01 +0800

    bump version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='hsdl',
-    version='2020.12.20e',
+    version='2020.12.22',
     author='timniven',
     author_email='tim.niven.public@gmail.com',
     description='Deep learning tools.',
