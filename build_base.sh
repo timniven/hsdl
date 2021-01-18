67f1f7fb77122498a815792da9d880c6bbd2f61b
hanshan <tim.niven.public@gmail.com>
2021-01-18 11:48:00 +0800

    script to download ckip

--- build_base.sh
+++ build_base.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+
+./download_ckip.sh
+
+
 docker build \
+    --no-cache \
     -f docker/hsdl-base.Dockerfile \
     -t timniven/hsdl:base \
     .
---
+++ download_ckip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! -d temp ]; then
+  echo "temp dir not found, creating..."
+  mkdir temp
+fi
+
+# check if data exists and return if so
+if [ ! -d temp/ckip ]; then
+  echo "downloading data..."
+  wget -O temp/ckip.zip http://ckip.iis.sinica.edu.tw/data/ckiptagger/data.zip
+  unzip temp/ckip.zip -d temp/ckip
+  rm temp/ckip.zip
+else
+  echo "Ckip data already downloaded."
+fi
